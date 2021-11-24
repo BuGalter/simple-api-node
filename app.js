90f305eaf97d853dd826750ef9d8555022510827
BuGalter <wardoc78@gmail.com>
2021-11-24 13:39:51 +0300

    Added to app.js simple server

---
+++ app.js
@@ -0,0 +1,50 @@
+const http = require('http')
+
+const host = '127.0.0.1'
+const port = 7000
+
+function notFound(res) {
+  res.statusCode = 404
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Not found\n')
+}
+
+const server = http.createServer((req, res) => {
+  switch (req.method) {
+    case 'GET': {
+      switch (req.url) {
+        case '/home': {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'text/plain')
+          res.end('Home page\n')
+          break
+        }
+        case '/about': {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'text/plain')
+          res.end('About page\n')
+          break
+        }
+        case '/api/data': {
+          res.statusCode = 200
+          res.setHeader('Content-Type', 'text/plain')
+          res.end('<h2 style="align:centre;">Return data<h2>')
+          break
+        }
+        default: {
+          notFound(res)
+          break
+        }
+      }
+      break
+    }
+    default: {
+      notFound(res)
+      break
+    }
+  }
+})
+
+server.listen(port, host, () => {
+  console.log(`Server listens http://${host}:${port}`)
+})
