a2bcc12872ac2ca1f74a06b06e93ae4eff8d45f4
BuGalter <wardoc78@gmail.com>
2021-11-24 16:33:26 +0300

    Added to test data, data for tests

--- test-data.js
+++ test-data.js
@@ -1,6 +1,33 @@
 const testData = [
   {
-    test: 'test'
+    date: '12.01.2021',
+    name: 'Брюки',
+    number: 10,
+    price: 1000
+  },
+  {
+    date: '15.02.2021',
+    name: 'Юбка',
+    number: 8,
+    price: 1200
+  },
+  {
+    date: '15.03.2021',
+    name: 'Платье',
+    number: 15,
+    price: 1500
+  },
+  {
+    date: '13.04.2021',
+    name: 'Шорты',
+    number: 10,
+    price: 950
+  },
+  {
+    date: '21.05.2021',
+    name: 'Носки',
+    number: 100,
+    price: 35
   },
 ];
 
