0529ec4fe06a270afcfe3fb0b7618d0a48ac3071
BuGalter <wardoc78@gmail.com>
2021-11-25 21:45:04 +0300

    Added to test data objects id

--- test-data.js
+++ test-data.js
@@ -1,29 +1,34 @@
 const testData = [
   {
+    id: 1,
     date: '12.01.2021',
     name: 'Брюки',
     number: 10,
     price: 1000
   },
   {
+    id: 2,
     date: '15.02.2021',
     name: 'Юбка',
     number: 8,
     price: 1200
   },
   {
+    id: 3,
     date: '15.03.2021',
     name: 'Платье',
     number: 15,
     price: 1500
   },
   {
+    id: 4,
     date: '13.04.2021',
     name: 'Шорты',
     number: 10,
     price: 950
   },
   {
+    id: 5,
     date: '21.05.2021',
     name: 'Носки',
     number: 100,
