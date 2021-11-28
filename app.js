c58416f639f9908c7171b0c4c119028ea75716b5
BuGalter <wardoc78@gmail.com>
2021-11-28 21:50:55 +0300

    Added to all routes return readme.md, exept route /api

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
 const config = require('./config');
 const data = require('./test-data');
 
+const path = require('path');
+
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || config.port;
 
 app.options('*', (req, res) => {
-  console.log(req.headers);
   res.status(200);
   res.setHeader({'Access-Control-Request-Method': 'GET',
                  'Access-Control-Allow-Headers': 'Content-Type'});
   res.end();
 });
 
-app.get('/', (req, res) => {
-  console.log(req.headers);
-  res.status(200).type('html')
-  res.send('<h1 style="color:red;text-align:center;">Hello from simple API</h1>');
-});
-
 app.get('/api', (req, res) => {
-  console.log(req.headers);
   res.status(200);
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200',);
   return res.json(data);
 });
+
+app.get('*', (req, res) => {
+  res.status(200).type('html')
+  res.sendFile(path.resolve(__dirname, 'README.md'));
+});
+
 
 app.listen(PORT, config.host, () => {
   console.log(`Simple API listening at http://${config.host}:${PORT}`);
