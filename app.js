573d460af7ed73a58be180a5c9fc99f839583d7a
BuGalter <wardoc78@gmail.com>
2021-11-28 21:43:47 +0300

    Added heandler for request with method option

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = express();
 
 const PORT = process.env.PORT || config.port;
 
+app.options('*', (req, res) => {
+  console.log(req.headers);
+  res.status(200);
+  res.setHeader({'Access-Control-Request-Method': 'GET',
+                 'Access-Control-Allow-Headers': 'Content-Type'});
+  res.end();
+});
+
 app.get('/', (req, res) => {
   console.log(req.headers);
   res.status(200).type('html')
@@ -13,7 +21,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api', (req, res) => {
+  console.log(req.headers);
   res.status(200);
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200',);
   return res.json(data);
 });
 
